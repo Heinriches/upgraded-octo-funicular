615c5b814759e993b604b7192949161dc7e72de9
Heinriches <heydrich.institut@gmail.com>
2026-04-02 21:41:37 -0300

    Update TicTacToe.java

--- 01/TicTacToe/Code/TicTacToe.java
+++ 01/TicTacToe/Code/TicTacToe.java
@@ -7,4 +7,22 @@ public class TicTacToe
 	{
 		System.out.println("Game is starting...");
 	}
+	
+	class Demo extends Frame implements ActionListener		// This class is the Window Blueprint
+	{
+		Demo()												// Class constructor-method
+		{
+			System.out.println("Building the window...");
+			setLayout(null);								// Manual placement of buttons
+			setSize(800, 600);								// Width and Height
+			setLocation(400, 100);							// Where it appears on your monitor
+			setBackground(Color.Black);						// Window background color
+			setVisible(true);								// Make it appear
+		}
+		
+		public void actionPerformed(ActionEvent e)			// This method is the "Ear", that listens for clicks 
+		{
+			// Logic goes here later
+		}
+	}
 }
